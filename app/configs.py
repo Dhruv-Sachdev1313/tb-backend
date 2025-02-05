b9adc0d9b554865da78e6e226e71056d712c789f
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2025-02-06 01:03:24 +0530

    configs
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- app/configs.py
+++ app/configs.py
@@ -1,5 +1,9 @@
 import os
+import dotenv
+
+dotenv.load_dotenv()
+
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/tick_data")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "postgresql://user:password@localhost/tick_data")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
