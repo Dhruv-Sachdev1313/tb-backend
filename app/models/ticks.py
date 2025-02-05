9c220fea1acd2f9696897885f30795972958f06c
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2025-02-06 01:03:42 +0530

    finalise models
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- app/models/ticks.py
+++ app/models/ticks.py
@@ -1,12 +1,24 @@
 from app import db
 
 class Tick(db.Model):
-    __tablename__ = "ticks"
-    timestamp = db.Column(db.DateTime, primary_key=True)
-    symbol = db.Column(db.String(10), primary_key=True)
-    price = db.Column(db.Float, nullable=False)
-    volume = db.Column(db.Integer, nullable=False)
-    exchange = db.Column(db.String(10))
+    __tablename__ = "ticker_data"
+    ts = db.Column(db.DateTime, primary_key=True)
+    ticker = db.Column(db.String(10), primary_key=True)
+    ltp = db.Column(db.Float, nullable=False)
+    ltq = db.Column(db.Integer, nullable=False)
+    buy_price = db.Column(db.Float, nullable=False)
+    buy_qty = db.Column(db.Integer, nullable=False)
+    sell_price = db.Column(db.Float, nullable=False)
+    sell_qty = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f"<Tick {self.symbol} {self.timestamp}>"
+        return f"<Tick {self.ticker} {self.ts}>"
+    
+class TickerMaster(db.Model):
+    __tablename__ = "tickers"
+    ticker = db.Column(db.String(10), primary_key=True)
+    exchange = db.Column(db.String(10), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    def __repr__(self):
+        return f"<TickerMaster {self.ticker}>"
